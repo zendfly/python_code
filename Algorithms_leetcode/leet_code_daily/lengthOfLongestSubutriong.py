54f7bb4155b6468fba4073cf9550082d08c5eebd
zendfly <zend_fly@163.com>
2019-11-25 22:41:13 +0800

    xiugai

--- Algorithms_leetcode/leet_code_daily/lengthOfLongestSubutriong.py
+++ Algorithms_leetcode/leet_code_daily/lengthOfLongestSubutriong.py
@@ -4,16 +4,18 @@
 class Solution:
     def lengthOflongestSubstring(self,s):
         if not s:return 0
-        left = 0
-        lookup = set()
+        left = 0    #用来计算
+        lookup = set()      #使用集合来存放子串
         n = len(s)
         max_len = 0
         cur_len = 0
         for i in range(n):
             cur_len += 1
+            #判断s[i]是否在lookup中，如果存在则把重复元素前的所有元素移除（包括重复的元素）
             while s[i] in lookup:
                 lookup.remove(s[left])
-                left +=1
+                #left表示滑窗的左边界，当出现重复元素时，移动左边界
+                left +=1    #用来表示要移除元素的下标，因为依次添加，故需要依次移除
                 cur_len -=1
             if cur_len > max_len:
                 max_len = cur_len
