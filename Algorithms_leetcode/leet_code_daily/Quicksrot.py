12222267339a7e3c3bda977c48d1bfe687769d0e
zendfly <zend_fly@163.com>
2019-11-26 21:55:43 +0800

    alter

--- Algorithms_leetcode/leet_code_daily/Quicksrot.py
+++ Algorithms_leetcode/leet_code_daily/Quicksrot.py
@@ -1,19 +1,22 @@
+#快排，选取一个pivot（基准）进行比较
+#1、大于pivot的放入右边，
+#2、小于的放入左边
+# 1和2 操作一个就行
+# 在进行交换时，以小于pivot交换到左边为例，当遇见小于pivot的元素时从下标为0的元素进行交换
 
-
+#使用
 def partition(arr,low,high):
-    i = (low-1)
-    pivot = arr[high]
+    i = (low-1)     #i用来计数，交换的位置
+    pivot = arr[high]       #pivot取数组最后一位
     for j in range(low,high):
         if arr[j]<= pivot:
-
             i = i+1
             arr[i],arr[j] = arr[j],arr[i]
 
-
-    arr[i+1],arr[high] = arr[high],arr[i+1]
-
+    arr[i+1],arr[high] = arr[high],arr[i+1]     #将pivot放入i+1的位置
     return i+1
 
+#递归调用交换函数
 def quickSort(arr, low, high):
     if low < high:
         pi = partition(arr, low, high)
